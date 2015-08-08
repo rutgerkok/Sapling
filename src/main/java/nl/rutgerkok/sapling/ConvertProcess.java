a25abd37014e19282d10a0339bef8c4b00eeb816
rutgerkok <rutger.kok@live.com>
2015-08-08 10:11:45 +0200

    Convert the level.dat file

--- src/main/java/nl/rutgerkok/sapling/ConvertProcess.java
+++ src/main/java/nl/rutgerkok/sapling/ConvertProcess.java
@@ -6,8 +6,10 @@ import java.util.Objects;
 import nl.rutgerkok.hammer.ChunkAccess;
 import nl.rutgerkok.hammer.anvil.AnvilChunk;
 import nl.rutgerkok.hammer.anvil.AnvilWorld;
+import nl.rutgerkok.hammer.anvil.tag.AnvilFormat.LevelTag;
 import nl.rutgerkok.hammer.pocket.PocketChunk;
 import nl.rutgerkok.hammer.pocket.PocketWorld;
+import nl.rutgerkok.hammer.tag.CompoundTag;
 import nl.rutgerkok.hammer.util.Progress;
 import nl.rutgerkok.hammer.util.Result;
 import nl.rutgerkok.hammer.util.Visitor;
@@ -28,6 +30,27 @@ final class ConvertProcess {
      *            Destination world, must be empty.
      */
     void convert(PocketWorld source, AnvilWorld destination) {
+        convertLevelDat(source, destination);
+        convertChunks(source, destination);
+    }
+
+    private void convertLevelDat(PocketWorld source, AnvilWorld destination) {
+        CompoundTag levelTag = destination.getLevelTag();
+        levelTag.clear();
+        levelTag.addAll(source.getLevelTag());
+
+        levelTag.setInt(LevelTag.VERSION, 19133); // Mark as Anvil map
+        // PE stores time as number of seconds since epoch, PC as milliseconds
+        levelTag.setLong(LevelTag.LAST_PLAYED, levelTag.getLong(LevelTag.LAST_PLAYED) * 1000);
+
+        try {
+            destination.saveLevelTag();
+        } catch (IOException e) {
+            errorLog.log("Conversion of level.dat failed", e);
+        }
+    }
+
+    private void convertChunks(PocketWorld source, AnvilWorld destination) {
         try (final ChunkAccess<AnvilChunk> chunkDestination = destination.getChunkAccess()) {
             final ChunkConverter chunkConverter = new ChunkConverter(errorLog);
 
@@ -48,7 +71,7 @@ final class ConvertProcess {
                 }
             });
         } catch (IOException e) {
-            errorLog.log("Conversion failed", e);
+            errorLog.log("Chunk conversion failed", e);
         }
     }
 
